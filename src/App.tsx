555fa33174244fc792b7b3d9dab8cb785805fbfb
Dinesh Rout <rddinesh779@gmail.com>
2023-02-08 01:16:37 +0530

    style: updated UI 💄

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className='bg-blue-700 min-h-[100vh] md:py-4'>
-      <div className='container md:w-[95%] mx-auto max-w-[1080px] p-3 md:p-6 rounded-md min-h-screen md:min-h-[95vh] bg-white'>
-        <Navbar />
-        <Outlet />
-      </div>
+    <div className='container md:w-[95%] mx-auto max-w-[1400px] p-3 md:p-6 rounded-md min-h-screen md:min-h-[80vh] bg-white'>
+      <Navbar />
+      <Outlet />
     </div>
   );
 }
