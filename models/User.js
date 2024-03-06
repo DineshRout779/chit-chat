e891551d36ea88896fc9cf949e8e64b907361c2f
Dinesh Rout <rddinesh779@gmail.com>
2024-03-06 17:47:21 +0530

    Model: added email field in user model

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
   {
     username: String,
+    email: String,
     password: String,
     profilePic: {
       type: String,
---
+++ utils/generateOTP.js
@@ -0,0 +1,7 @@
+function generateOTP() {
+  // Generate a random 6-digit number
+  const otp = Math.floor(100000 + Math.random() * 900000);
+  return otp;
+}
+
+export default generateOTP;
