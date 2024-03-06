534569892e05c36faa89bc7e4d6046b74906120a
Dinesh Rout <rddinesh779@gmail.com>
2024-03-06 17:01:27 +0530

    refactor: updated UI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Login = () => {
   const { state, loginUser } = useAuth();
@@ -47,50 +48,67 @@ const Login = () => {
   return (
     <div className='flex justify-center items-center'>
       <div className='md:basis-1/2 bg-blue-600 min-h-screen'></div>
-      <div className='w-[90%] md:basis-1/2 p-4 px-8 mx-auto max-w-[480px]'>
-        <h1 className='text-2xl font-medium'>Login to continue</h1>
-        <form className='block w-full' onSubmit={handleSubmit}>
-          <input
-            type='text'
-            name='username'
-            id='username'
-            value={username}
-            onChange={handleInputChange}
-            className='py-3 px-4 my-4 block w-full border outline-none border-gray-500 rounded-md text-sm focus:border-blue-500  disabled:opacity-50 disabled:pointer-events-none dark:bg-zinc-900 dark:border-gray-700 dark:text-gray-400 dark:focus:border-gray-600'
-            placeholder='Enter username'
-          />
+      <div className='md:basis-1/2 w-full flex justify-center items-center bg-white/75 dark:bg-black/75 backdrop-blur-3xl min-h-screen'>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{
+            opacity: {
+              ease: 'easeIn',
+              duration: 1,
+            },
+          }}
+          className='w-[90%] p-4 px-8 mx-auto max-w-[480px]'
+        >
+          <h1 className='text-2xl font-medium dark:text-gray-200'>Login</h1>
+          <p className='text-sm dark:text-gray-400'>Login to continue </p>
+          <form className='block w-full' onSubmit={handleSubmit}>
+            <input
+              type='text'
+              name='username'
+              id='username'
+              value={username}
+              onChange={handleInputChange}
+              className='py-3 px-4 my-4 block w-full border outline-none border-gray-500 rounded-md text-sm focus:border-blue-500  disabled:opacity-50 disabled:pointer-events-none dark:bg-zinc-900 dark:border-gray-700 dark:text-gray-400 dark:focus:border-gray-600'
+              placeholder='Enter username'
+            />
 
-          {/* password */}
-          <input
-            type='password'
-            name='password'
-            id='password'
-            value={password}
-            onChange={handleInputChange}
-            className='py-3 px-4 my-4 block w-full border outline-none border-gray-500 rounded-md text-sm focus:border-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-zinc-900 dark:border-gray-700 dark:text-gray-400 dark:focus:border-gray-600'
-            placeholder='Enter password'
-          />
-          <button
-            type='submit'
-            className='block p-2 my-4 w-full rounded-md bg-blue-600 text-white'
-          >
-            Login
-          </button>
-          <button
-            type='button'
-            onClick={getGuestCredentials}
-            className='block p-2 my-4 w-full rounded-md bg-gray-400 dark:bg-zinc-600 text-white'
-          >
-            Login as guest
-          </button>
-        </form>
-        <p>
-          Don&apos;t have an account?
-          <Link className='text-blue-600' to='/signup'>
-            {' '}
-            Create an account
-          </Link>
-        </p>
+            {/* password */}
+            <input
+              type='password'
+              name='password'
+              id='password'
+              value={password}
+              onChange={handleInputChange}
+              className='py-3 px-4 my-4 block w-full border outline-none border-gray-500 rounded-md text-sm focus:border-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-zinc-900 dark:border-gray-700 dark:text-gray-400 dark:focus:border-gray-600'
+              placeholder='Enter password'
+            />
+            <Link to='/forget-password' className='text-sm text-blue-400'>
+              Forgot password
+            </Link>
+            <button
+              type='submit'
+              className='block p-2 my-4 w-full rounded-md bg-blue-600 text-white'
+            >
+              Login
+            </button>
+            <button
+              type='button'
+              onClick={getGuestCredentials}
+              className='block p-2 my-4 w-full rounded-md bg-gray-400 dark:bg-zinc-600 text-white'
+            >
+              Login as guest
+            </button>
+          </form>
+          <p className='dark:text-gray-200'>
+            Don&apos;t have an account?
+            <Link className='text-blue-600' to='/signup'>
+              {' '}
+              Create an account
+            </Link>
+          </p>
+        </motion.div>
       </div>
     </div>
   );
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,16 +1,18 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
+import apiClient from '../services/apiClient';
+import { motion } from 'framer-motion';
 
 const Signup = () => {
   const { state } = useAuth();
   const navigate = useNavigate();
   const [values, setValues] = useState({
     username: '',
+    email: '',
     password: '',
   });
-  const { username, password } = values;
+  const { username, email, password } = values;
 
   const handleInputChange = (e) => {
     setValues({
@@ -23,7 +25,7 @@ const Signup = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post('http://localhost:3000/auth/signup', values);
+      const res = await apiClient.post('/auth/signup', values);
       if (res.status === 201) {
         navigate('/');
       }
@@ -39,35 +41,59 @@ const Signup = () => {
   return (
     <div className='flex justify-center items-center'>
       <div className='md:basis-1/2 bg-blue-600 min-h-screen'></div>
-      <div className='w-[90%] md:basis-1/2 p-4 px-8 mx-auto max-w-[480px]'>
-        <h1 className='text-2xl mb-4 font-medium dark:text-white'>
-          Login to continue
-        </h1>
-        <form className='block w-full' onSubmit={handleSubmit}>
-          {/* username */}
-          <input
-            type='text'
-            name='username'
-            id='username'
-            value={username}
-            onChange={handleInputChange}
-            className='py-3 px-4 my-4 block w-full border outline-none border-gray-500 rounded-md text-sm focus:border-blue-500  disabled:opacity-50 disabled:pointer-events-none dark:bg-zinc-900 dark:border-gray-700 dark:text-gray-400 dark:focus:border-gray-600'
-            placeholder='Enter username'
-          />
+      <div className='md:basis-1/2 w-full flex justify-center items-center bg-white/75 dark:bg-black/75 backdrop-blur-3xl min-h-screen'>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{
+            opacity: {
+              ease: 'easeIn',
+              duration: 1,
+            },
+          }}
+          className='w-[90%] p-4 px-8 mx-auto max-w-[480px]'
+        >
+          <h1 className='text-2xl my-2 font-medium dark:text-white'>Signup</h1>
+          <p className='text-sm dark:text-gray-400'>
+            Create an account to get started
+          </p>
+          <form className='block w-full' onSubmit={handleSubmit}>
+            {/* username */}
+            <input
+              type='text'
+              name='username'
+              id='username'
+              value={username}
+              onChange={handleInputChange}
+              className='py-3 px-4 my-4 block w-full border outline-none border-gray-500 rounded-md text-sm focus:border-blue-500  disabled:opacity-50 disabled:pointer-events-none dark:bg-zinc-900 dark:border-gray-700 dark:text-gray-400 dark:focus:border-gray-600'
+              placeholder='Enter username'
+            />
 
-          {/* password */}
-          <input
-            type='password'
-            name='password'
-            id='password'
-            value={password}
-            onChange={handleInputChange}
-            className='py-3 px-4 my-4 block w-full border outline-none border-gray-500 rounded-md text-sm focus:border-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-zinc-900 dark:border-gray-700 dark:text-gray-400 dark:focus:border-gray-600'
-            placeholder='Enter password'
-          />
+            {/* email */}
+            <input
+              type='email'
+              name='email'
+              id='email'
+              value={email}
+              onChange={handleInputChange}
+              className='py-3 px-4 my-4 block w-full border outline-none border-gray-500 rounded-md text-sm focus:border-blue-500  disabled:opacity-50 disabled:pointer-events-none dark:bg-zinc-900 dark:border-gray-700 dark:text-gray-400 dark:focus:border-gray-600'
+              placeholder='Enter email'
+            />
 
-          {/* profile picture */}
-          {/* <label htmlFor='small-file-input' className='sr-only'>
+            {/* password */}
+            <input
+              type='password'
+              name='password'
+              id='password'
+              value={password}
+              onChange={handleInputChange}
+              className='py-3 px-4 my-4 block w-full border outline-none border-gray-500 rounded-md text-sm focus:border-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-zinc-900 dark:border-gray-700 dark:text-gray-400 dark:focus:border-gray-600'
+              placeholder='Enter password'
+            />
+
+            {/* profile picture */}
+            {/* <label htmlFor='small-file-input' className='sr-only'>
             Choose file
           </label>
           <input
@@ -81,20 +107,21 @@ const Signup = () => {
             dark:file:bg-black dark:file:text-gray-200'
             placeholder='Upload'
           /> */}
-          <button
-            type='submit'
-            className='block p-2 my-4 w-full rounded-md bg-blue-600 text-white'
-          >
-            Create account
-          </button>
-        </form>
-        <p className='dark:text-gray-200'>
-          Already have an account?{' '}
-          <Link className='text-blue-600 ' to='/'>
-            {' '}
-            Login
-          </Link>
-        </p>
+            <button
+              type='submit'
+              className='block p-2 my-4 w-full rounded-md bg-blue-600 text-white'
+            >
+              Create account
+            </button>
+          </form>
+          <p className='dark:text-gray-200'>
+            Already have an account?{' '}
+            <Link className='text-blue-600 ' to='/'>
+              {' '}
+              Login
+            </Link>
+          </p>
+        </motion.div>
       </div>
     </div>
   );
