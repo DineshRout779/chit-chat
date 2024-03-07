15f7a564f21bd3fdcd440b1c48f5fb5b63724fd3
Dinesh Rout <rddinesh779@gmail.com>
2024-03-07 11:42:37 +0530

    fix: token shows empty after login

--- client/src/services/apiClient.js
+++ client/src/services/apiClient.js
@@ -1,19 +1,37 @@
 import axios from 'axios';
 
+// Retrieve token from localStorage
+const getToken = () => {
+  return localStorage.getItem('token') || null;
+};
+
+// Set up API client
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('token') || null}`,
   },
 });
 
+// Configure the API request
+apiClient.interceptors.request.use(
+  (config) => {
+    // Update Authorization header with the latest token before each request
+    config.headers.Authorization = `Bearer ${getToken()}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+//  Configure the API response
 apiClient.interceptors.response.use(
-  function (response) {
+  (response) => {
     return response;
   },
-  function (error) {
+  (error) => {
     let res = error.response;
     console.error(`Looks like there was a problem. Status Code: ${res.status}`);
     return Promise.reject(error);
