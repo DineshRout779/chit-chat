3866b10f817d818a562c406015cc605664406f8e
Dinesh Rout <rddinesh779@gmail.com>
2024-03-12 11:45:01 +0530

    fix: added Mongo URI through env

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/chat');
+    await mongoose.connect(process.env.MONGO_URL);
 
     console.log('Database connected');
   } catch (error) {
